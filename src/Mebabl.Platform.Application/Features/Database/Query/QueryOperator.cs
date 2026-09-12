53c8b82b64361871df9b595dd6378a1aed819fd3
Ali Mirdan <alimirdan02@gmail.com>
2026-09-12 03:28:29 +0300

    Fix

--- src/Mebabl.Platform.Application/Features/Database/Query/QueryOperator.cs
+++ src/Mebabl.Platform.Application/Features/Database/Query/QueryOperator.cs
@@ -1,5 +1,8 @@
+using System.Text.Json.Serialization;
+
 namespace Mebabl.Platform.Application.Features.Database.Query;
 
+[JsonConverter(typeof(JsonStringEnumConverter))]
 public enum QueryOperator
 {
     Equal,
