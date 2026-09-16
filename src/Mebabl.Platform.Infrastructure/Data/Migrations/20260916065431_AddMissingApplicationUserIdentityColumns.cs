2daf3d008f0c800b116b3136be4deb74ec20f184
Ali Mirdan <alimirdan02@gmail.com>
2026-09-16 10:17:04 +0300

    Fix application user identity migration

---
+++ src/Mebabl.Platform.Infrastructure/Data/Migrations/20260916065431_AddMissingApplicationUserIdentityColumns.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Mebabl.Platform.Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddMissingApplicationUserIdentityColumns : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
